68c31e34a3074189a7906327e33bd8ddd7d5ece8
rahul2289 <burrarahulgoud1999@gmail.com>
2022-03-03 16:50:55 +0530

    graphy api fetched

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
   const [text, setText] = useState("");
   const [message, setMessage] = useState([]);
   const [style, setStyle] = useState(true);
+  const [title, setTitle] = useState("");
+  const [data, setData] = useState([]);
+
+  const ApiKey = "BnB60B1zI4Hr99UlTRRpBrMEn97ECvmz";
+  const fectchgif = async (title) => {
+    const response = await axios
+      .get(
+        `https://api.giphy.com/v1/gifs/search?q=${title}&api_key=${ApiKey}&limit=10`
+      )
+      .then((res) => setData(res.data.data));
+    console.log(response);
+  };
+
+  useEffect(() => {
+    fectchgif(title);
+  }, [title]);
+
   const handleChange = (e) => {
     setText(e.target.value);
   };
@@ -25,10 +43,24 @@ function App() {
         text={text}
         handleSubmit={handleSubmit}
       />
-      <div>
-        <h5 onClick={changestyle}>gift</h5>
-        <input type="text" className={style ? "none" : "block"} />
+      <div className="gift-container">
+        <h5 onClick={changestyle}>Gift</h5>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          className={style ? "none" : "block"}
+        />
       </div>
+      {data.map((gift) => {
+        return (
+          <div key={gift.id} className={style ? "none" : "block"}>
+            <img src={gift.images.fixed_width.url} alt={gift.title} />
+          </div>
+        );
+      })}
       <List message={message} />
     </div>
   );
@@ -51,7 +83,11 @@ export const List = ({ message }) => {
   return (
     <div>
       {message.map((item, i) => {
-        return <h1 key={i}>{item}</h1>;
+        return (
+          <div key={i} className="box">
+            <h3>{item}</h3>
+          </div>
+        );
       })}
     </div>
   );
